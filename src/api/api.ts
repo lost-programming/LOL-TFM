20cbd087843871d90292cb8253249e9c05f874ed
dnwnvus <dnwnvus@gmail.com>
2023-11-21 17:02:13 +0900

    commit

---
+++ src/api/api.ts
@@ -0,0 +1,5 @@
+// name: 편안한승차감
+// accountId: "GK9UlszdC0UQiD5Imbd0ZgPy9nS8umi1CNycEkQercxAYuc"
+// puuId: "QAy6EntIEtEYXWL8y3PMh9giUQZrQ7qRU1bUZKZrjMFC7Z4gztP3qHuoRL4LujSJSXF0o41fF8lErw"
+// 챔피언 전체 URL: https://ddragon.leagueoflegends.com/cdn/13.22.1/data/en_US/champion.json
+// 챔피언 예시 URL: https://ddragon.leagueoflegends.com/cdn/13.22.1/data/en_US/champion/Aatrox.json
