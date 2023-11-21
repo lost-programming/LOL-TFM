577cd69b64f171b703bc00780f3d9cb87dc4aaec
dnwnvus <dnwnvus@gmail.com>
2023-11-21 20:49:00 +0900

    mui 오류 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import Search from "@/components/search/search";
+import { styled } from "@mui/material";
 import React from "react";
 
+const MainContainer = styled("div")({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
 const Home = () => {
-  return <></>;
+  return (
+    <MainContainer>
+      <Search placeholder="챔피언 검색" />
+    </MainContainer>
+  );
 };
 
 export default Home;
--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
 
-const Search = () => {
-  return <></>;
+import React from "react";
+import { InputBase, Paper, styled } from "@mui/material";
+
+interface SearchProps {
+  placeholder: string;
+}
+
+const SearchContainer = styled(Paper)({
+  display: "flex",
+  alignItems: "center",
+});
+
+const SearchInput = styled(InputBase)({
+  width: 400,
+  padding: 7,
+});
+
+const Search = ({ placeholder }: SearchProps) => {
+  return (
+    <SearchContainer>
+      <SearchInput
+        sx={{ ml: 1, flex: 1 }}
+        autoFocus={true}
+        placeholder={placeholder}
+        type="search"
+      ></SearchInput>
+    </SearchContainer>
+  );
 };
 
 export default Search;
