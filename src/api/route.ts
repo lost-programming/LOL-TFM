d82174e30c743efe83f62932dcee272700262b68
dnwnvus <82806037+dnwnvus@users.noreply.github.com>
2023-12-01 23:26:34 +0900

    Delete src/api directory

--- src/api/route.ts
+++
@@ -1,6 +0,0 @@
-// name: 편안한승차감
-// accountId: "GK9UlszdC0UQiD5Imbd0ZgPy9nS8umi1CNycEkQercxAYuc"
-// puuId: "QAy6EntIEtEYXWL8y3PMh9giUQZrQ7qRU1bUZKZrjMFC7Z4gztP3qHuoRL4LujSJSXF0o41fF8lErw"
-// 챔피언 전체 URL: https://ddragon.leagueoflegends.com/cdn/13.22.1/data/en_US/champion.json
-// 챔피언 예시 URL: https://ddragon.leagueoflegends.com/cdn/13.22.1/data/en_US/champion/Aatrox.json
-// Riot Personal api : RGAPI-f1f52807-543c-401c-afd5-190818d58cc2
